00c577cac37a83ea2934bc16c8973a108eeb7342
Zhi Yin <Z.Yin.MD@gmail.com>
2020-03-27 22:38:01 -0400

    add demo4

--- src/Playground.js
+++ src/Playground.js
@@ -10,17 +10,17 @@ export const playground = {
     });
     openDB('db2', 1, {
       upgrade(db) {
-        db.createObjectStore('store3');
-        db.createObjectStore('store4');
+        db.createObjectStore('store3', { keyPath: 'id' });
+        db.createObjectStore('store4', { autoIncrement: true });
       },
     });
   },
-  async 'demo2: Add some key values pairs in store1'() {
+  async 'demo2: add some key values pairs in store1'() {
     const db1 = await openDB('db1', 1);
     db1.add('store1', 'hello world', 'message');
     db1.add('store1', true, 'delivered');
   },
-  async 'demo3: Error handling'() {
+  async 'demo3: error handling'() {
     const db1 = await openDB('db1', 1);
     db1
       .add('store1', 'hello again!!', 'new message')
@@ -30,5 +30,12 @@ export const playground = {
       .catch(err => {
         console.error('new message error: ', err);
       });
+  },
+  async 'demo4: auto generate keys'() {
+    const db2 = await openDB('db2', 1);
+    db2.add('store3', { id: 'cat001', strength: 10, speed: 10 });
+    db2.add('store3', { id: 'cat002', strength: 11, speed: 9 });
+    db2.add('store4', { id: 'cat003', strength: 8, speed: 12 });
+    db2.add('store4', { id: 'cat004', strength: 12, speed: 13 });
   },
 };
