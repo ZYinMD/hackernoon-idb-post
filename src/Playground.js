2026e0366a3193f33e5484ce98e92fe7a9c4e18e
Zhi Yin <Z.Yin.MD@gmail.com>
2020-03-29 12:18:19 -0400

    add db.close() to all functions

--- src/Playground.js
+++ src/Playground.js
@@ -19,6 +19,7 @@ export const playground = {
     const db1 = await openDB('db1', 1);
     db1.add('store1', 'hello world', 'message');
     db1.add('store1', true, 'delivered');
+    db1.close();
   },
   async 'demo3: error handling'() {
     const db1 = await openDB('db1', 1);
@@ -30,6 +31,7 @@ export const playground = {
       .catch(err => {
         console.error('new message error: ', err);
       });
+    db1.close();
   },
   async 'demo4: auto generate keys'() {
     const db2 = await openDB('db2', 1);
@@ -37,6 +39,7 @@ export const playground = {
     db2.add('store3', { id: 'cat002', strength: 11, speed: 9 });
     db2.add('store4', { id: 'cat003', strength: 8, speed: 12 });
     db2.add('store4', { id: 'cat004', strength: 12, speed: 13 });
+    db2.close();
   },
   async 'demo5: retrieve values'() {
     const db2 = await openDB('db2', 1);
@@ -46,15 +49,17 @@ export const playground = {
     db2.getAll('store3').then(console.log);
     // count the totle number or items in a store
     db2.count('store3').then(console.log);
+    db2.close();
   },
   async 'demo6: replace item with same key'() {
     // set db1/store1/delivered to be false:
     const db1 = await openDB('db1', 1);
     db1.put('store1', false, 'delivered');
-
+    db1.close();
     // replace cat001 with a supercat
     const db2 = await openDB('db2', 1);
     db2.put('store3', { id: 'cat001', strength: 99, speed: 99 });
+    db2.close();
   },
   async 'demo7: multiple operations within one transaction'() {
     const db2 = await openDB('db2', 1);
@@ -64,6 +69,7 @@ export const playground = {
     let superCat = await transaction.objectStore('store3').get('cat001');
     transaction.objectStore('store3').delete('cat001');
     transaction.objectStore('store4').add(superCat);
+    db2.close();
   },
   async 'demo8: transaction on a single store, and error handling'() {
     // we'll only operate on one store this time:
@@ -81,6 +87,7 @@ export const playground = {
       .catch(() => {
         console.error('Something went wrong, transaction aborted');
       });
+    db1.close();
   },
   async 'demo9: very explicitly create a new db and new store'() {
     const db3 = await openDB('db3', 1, {
